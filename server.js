83384481e603a81b554bf5a2081f1097c44f2961
Myeager86 <31748384+Myeager86@users.noreply.github.com>
2018-05-19 11:40:32 -0600

    removed odd dependency

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
 
+// Sets up the Express App
+// =============================================================
 var app = express();
+var PORT = process.env.PORT || 8080;
 
+// Requiring our models for syncing
 var db = require("./models");
 
-// // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());
+// Sets up the Express app to handle data parsing
+
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// parse application/json
+app.use(bodyParser.json());
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + "/public"));
+// Static directory
+app.use(express.static("public"));
 
-// // override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
+// Routes
+// =============================================================
+// require("./routes/html-routes.js")(app);
+// require("./routes/author-api-routes.js")(app);
+// require("./routes/post-api-routes.js")(app);
 
-// initiate handlebars with default layout
-var exphbs = require("express-handlebars");
-app.engine(
-    "handlebars",
-    exphbs({
-        defaultLayout: "main"
-    })
-);
-app.set("view engine", "handlebars");
-
-// establish api routes for database access
-require("./routes/api-routes.js")(app);
-
-var port = process.env.PORT || 3000;
-// connect to database, sync with database, then listen on port 3000
-db.sequelize.sync().then(() => {
-    app.listen(port, () => {
-        console.log(`Listening on port ${port}`);
-    });
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
