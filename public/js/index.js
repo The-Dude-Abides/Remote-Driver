e17c4e456eba6aa27bb385b886c40fd8c1ba0de4
Myeager86 <31748384+Myeager86@users.noreply.github.com>
2018-05-19 11:30:26 -0600

    added package.json

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    const forward = $("#up-button");
+    const right = $("#right-button");
+    const left = $("#left-button");
+    const back = $("#down-button");
+
+    
+});
