c2f5c085badde38b2b0607d74c8fb1cc6f763bc2
Koltyn Palmer <koltynchiaro@gmail.com>
2018-05-23 00:20:55 -0600

    js file now connected...

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,25 @@
+
+
 $(document).ready(function() {
 
-    const commandLog = $("#command-log");
-    const forward = $("#up-button")
+    const commandLog = $("#commandlog");
+    const forward = $("#upbutton")
             // .val().appendTo(commandLog);
-    const right = $("#right-button");
-    var left = $("#left-button");
-    const back = $("#down-button");
-    
-
+    const right = $("#rightbutton");
+    // var left = $("#left-button");
+    const back = $("#downbutton");
+    // $('#left-button').click(function() {
+    //     var clicker = $(this).attr('data-point');
+    //     console.log(clicker);
+    // })
 });
 
-$('#left-button').click(function() {
-    var clicker = $(this).attr('data-point');
-    console.log(clicker);
+$(document).ready(function() {
+$(".").click(function() {
+    // var clicker = $("#left-button").attr( "data-point" ).text();
+    console.log("clicked: ");
 })
+
+});
 
 console.log("what")
