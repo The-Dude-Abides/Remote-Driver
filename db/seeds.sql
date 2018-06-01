919281a07348bfbd2043bddfbff3a66ed7122a45
Koltyn Palmer <koltynchiaro@gmail.com>
2018-06-01 16:14:51 -0700

    seeds schema button name

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,6 +5,7 @@ CREATE TABLE events
 (
 	id int NOT NULL AUTO_INCREMENT,
 	user_name varchar(255) NOT NULL,
+    button_name varchar(255) NOT NULL,
     button_direction varchar(255) NOT NULL,
     button_degree int NOT NULL,
     current_direction varchar(255) NOT NULL,
@@ -15,42 +16,49 @@ CREATE TABLE events
 
 
 INSERT INTO events (user_name) VALUES ('koltp')
+INSERT INTO events (button_name) VALUES ('down-button')
 INSERT INTO events (button_direction) VALUES ('backward')
 INSERT INTO events (button_degree) VALUES ('-180')
 INSERT INTO events (current_direction) VALUES ('backward')
 INSERT INTO events (current_degree) VALUES ('-180');
 
 INSERT INTO events (user_name) VALUES ('koltp');
+INSERT INTO events (button_name) VALUES ('up-button');
 INSERT INTO events (button_direction) VALUES ('forward');
 INSERT INTO events (button_degree) VALUES ('180');
 INSERT INTO events (current_direction) VALUES ('forward');
 INSERT INTO events (current_degree) VALUES ('0');
 
 INSERT INTO events (user_name) VALUES ('koltp');
+INSERT INTO events (button_name) VALUES ('down-button');
 INSERT INTO events (button_direction) VALUES ('backward');
 INSERT INTO events (button_degree) VALUES ('-180');
 INSERT INTO events (current_direction) VALUES ('forward');
 INSERT INTO events (current_degree) VALUES ('-180');
 
 INSERT INTO events (user_name) VALUES ('koltp');
+INSERT INTO events (button_name) VALUES ('right-button');
 INSERT INTO events (button_direction) VALUES ('right');
 INSERT INTO events (button_degree) VALUES ('90');
 INSERT INTO events (current_direction) VALUES ('left');
 INSERT INTO events (current_degree) VALUES ('-90');
 
 INSERT INTO events (user_name) VALUES ('koltp');
+INSERT INTO events (button_name) VALUES ('right-button');
 INSERT INTO events (button_direction) VALUES ('right');
 INSERT INTO events (button_degree) VALUES ('90');
 INSERT INTO events (current_direction) VALUES ('forward');
 INSERT INTO events (current_degree) VALUES ('0');
 
 INSERT INTO events (user_name) VALUES ('koltp');
+INSERT INTO events (button_name) VALUES ('left-button');
 INSERT INTO events (button_direction) VALUES ('left');
 INSERT INTO events (button_degree) VALUES ('-90');
 INSERT INTO events (current_direction) VALUES ('left');
 INSERT INTO events (current_degree) VALUES ('-90');
 
 INSERT INTO events (user_name) VALUES ('koltp');
+INSERT INTO events (button_name) VALUES ('left-button');
 INSERT INTO events (button_direction) VALUES ('left');
 INSERT INTO events (button_degree) VALUES ('-90');
 INSERT INTO events (current_direction) VALUES ('backward');
