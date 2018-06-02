6e30a4ad888e16f11529051f2df9eb32028e150e
Myeager86 <31748384+Myeager86@users.noreply.github.com>
2018-06-01 21:15:46 -0600

    get and post api routes up

---
+++ routes/movement-api-routes..js
@@ -0,0 +1,20 @@
+var db = require("../models");
+
+module.exports = function (app) {
+    app.get("/api/movement", function (req, res) {
+        // Here we add an "include" property to our options in our findAll query
+        // We set the value to an array of the models we want to include in a left outer join
+        // In this case, just db.Post
+        db.movement.findAll({})
+            .then(function (dbMovement) {
+             res.json(dbMovement);
+        });
+    });
+
+    app.post("/api/movement", function(req, res) {
+        db.movement.create(req.body).then(function(dbMovement) {
+          res.json(dbMovement);
+        });
+      });
+
+}
