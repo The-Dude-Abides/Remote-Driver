b3c96e926f9bf0c10ecdc54fe7dfd8964338b790
Koltyn Palmer <koltynchiaro@gmail.com>
2018-06-02 08:05:30 -0600

    green theme

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
-var angle;
 
 $(document).ready(function() {
     
+    var angle;
     const forward = $("#up-button")
     const right = $("#right-button");
     const left = $("#left-button");
@@ -14,12 +14,15 @@ $(document).ready(function() {
         var name = $(this).attr("name");
         var direction = $(this).attr("data-direction");
         var degrees = $(this).attr("data-degrees");
-        console.log("clicked: " + name+"\ndirection: " + direction+"\ndegrees: " + degrees);
-        var metaSample = ("<div><br>clicked: " + name+"<br>direction: " + direction+"<br>degrees: " + degrees + "<br></div>");
+        console.log("clicked: " + name + "\ndirection: " + direction + "\ndegrees: " + degrees);
+        var metaSample = ("<div> <br> clicked: " + name + 
+                                "<br> direction: " + direction + 
+                                "<br> degrees: " + degrees + 
+                                "<br> </div>");
         
-        var glyph = $("<span id='glyph-"+direction+"'r class='glyphicon glyphicon-arrow-"+direction+" glyph-"+direction+"'></span>");
-        $("#compass").empty().append(glyph);
-        $("#command-log").append(metaSample);
+        var glyph = $("<span id='glyph-" + direction + "'r class='glyphicon glyphicon-arrow-" + direction + " glyph-" + direction+"'></span>");
+                    $("#compass").empty().append(glyph).addClass("compass-glyph");;
+                    $("#command-log").append(metaSample);
     
         
     })
@@ -32,109 +35,3 @@ $(document).ready(function() {
 
 console.log("drive safe ;p")
 
-        // >>>>>>> c0343bca79133ca1e1919836ee4e937fc3c86cbe
-//     var canvas = document.getElementById('game');
-//     var context = canvas.getContext('2d');
-//     var grid = 16;
-//     var snake = {
-//         x: 160,
-//         y: 160,
-//         dx: grid,
-//         dy: 0,
-//         cells: [],
-//         maxCells: 4
-//     };
-//     var count = 0;
-//     var apple = {
-//         x: 320,
-//         y: 320
-//     };
-//     function getRandomInt(min, max) {
-//         return Math.floor(Math.random() * (max - min)) + min;
-//     }
-//     // game loop
-//     function loop() {
-//         requestAnimationFrame(loop);
-//         // slow game loop to 15 fps instead of 60 - 60/15 = 4
-//         if (++count < 4) {
-//             return;
-//         }
-//         count = 0;
-//         context.clearRect(0, 0, canvas.width, canvas.height);
-//         snake.x += snake.dx;
-//         snake.y += snake.dy;
-//         // wrap snake position on edge of screen
-//         if (snake.x < 0) {
-//             snake.x = canvas.width - grid;
-//         }
-//         else if (snake.x >= canvas.width) {
-//             snake.x = 0;
-//         }
-//         if (snake.y < 0) {
-//             snake.y = canvas.height - grid;
-//         }
-//         else if (snake.y >= canvas.height) {
-//             snake.y = 0;
-//         }
-//         // keep track of where snake has been. front of the array is always the head
-//         snake.cells.unshift({ x: snake.x, y: snake.y });
-//         // remove cells as we move away from them
-//         if (snake.cells.length > snake.maxCells) {
-//             snake.cells.pop();
-//         }
-//         // // draw apple
-//         // context.fillStyle = 'red';
-//         // context.fillRect(apple.x, apple.y, grid - 1, grid - 1);
-//         // draw snake
-//         context.fillStyle = 'green';
-//         snake.cells.forEach(function (cell, index) {
-//             context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
-//             // snake ate apple
-//             if (cell.x === apple.x && cell.y === apple.y) {
-//                 snake.maxCells++;
-//                 apple.x = getRandomInt(0, 25) * grid;
-//                 apple.y = getRandomInt(0, 25) * grid;
-//             }
-//             // check collision with all cells after this one (modified bubble sort)
-//             for (var i = index + 1; i < snake.cells.length; i++) {
-
-//                 // collision. reset game
-//                 if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
-//                     snake.x = 160;
-//                     snake.y = 160;
-//                     snake.cells = [];
-//                     snake.maxCells = 4;
-//                     snake.dx = grid;
-//                     snake.dy = 0;
-//                     apple.x = getRandomInt(0, 25) * grid;
-//                     apple.y = getRandomInt(0, 25) * grid;
-//                 }
-//             }
-//         });
-//     }
-//     document.addEventListener('keydown', function (e) {
-//         // prevent snake from backtracking on itself
-//         if (e.which === 37 && snake.dx === 0 || back) {
-//             snake.dx = -grid;
-//             snake.dy = 0;
-//         }
-//         else if (e.which === 38 && snake.dy === 0) {
-//             snake.dy = -grid;
-//             snake.dx = 0;
-//         }
-//         else if (e.which === 39 && snake.dx === 0) {
-//             snake.dx = grid;
-//             snake.dy = 0;
-//         }
-//         else if (e.which === 40 && snake.dy === 0) {
-//             snake.dy = grid;
-//             snake.dx = 0;
-//         }
-//     });
-//     requestAnimationFrame(loop);
-
-//     forward.click(function() {
-//             snake.dx = -grid;
-//             snake.dy = 0;
-//     });
-// >>>>>>> c0343bca79133ca1e1919836ee4e937fc3c86cbe
